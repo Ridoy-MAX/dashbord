4f59e93c7fe34c217eaf4cf78771cdd47524e438
Ridoy-MAX <pou4w98y@gmail.com>
2021-11-29 01:53:04 +0600

    reply button problm fix

--- assets/js/coustom.js
+++ assets/js/coustom.js
@@ -176,162 +176,344 @@ button.onclick = function(){
 //me reply end===============================================================
 
 
-// reply section  two=======================================================
-var contant = document .getElementById("contant1");
-var button1 = document .getElementById("close1");
-button1.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
-
-    }
-    else{
-        contant.className = " hidden";
-    }
-}
 
 
 
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply6");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply7");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply8");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply9");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply10");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply11");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply12");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply13");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply14");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply15");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply16");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
-    }
-    else{
-        contant.className = " show1";
-    }
-}
-// reply==============
-var contant = document .getElementById("contant1");
-var button = document .getElementById("reply17");
-button.onclick = function(){
-    if(contant.className == "hidden"){
-        contant.className = "";
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//reply section  two=======================================================
+//reply==============
+
+
+
+
+
+
+//  reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply7");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply8");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply9");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply10");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply11");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply12");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply13");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply14");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply15");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply16");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+// // reply==============
+// var contant = document .getElementById("contant1");
+// var button = document .getElementById("reply17");
+// button.onclick = function(){
+//     if(contant.className == "hidden"){
+//         contant.className = "";
+
+//     }
+//     else{
+//         contant.className = " show1";
+//     }
+// }
+
+
+
+//close funtion
+$(document).ready(function(){ 
+
+    var contant = document .getElementById("contant-two");
+    var button1 = document .getElementById("close1");
+    button1.onclick = function(){
+        if(contant.className == "show1"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " hidden";
+        }
     }
-    else{
-        contant.className = " show1";
+
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply6");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
     }
-}
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply7");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply8");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply9");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply10");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply11");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply12");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply13");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply14");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply15");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply16");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+     //reply----  
+    var contant = document .getElementById("contant-two");
+    var button = document .getElementById("reply17");
+    button.onclick = function(){
+        if(contant.className == "hidden-two"){
+            contant.className = "";
+    
+        }
+        else{
+            contant.className = " show-two";
+        }
+    }
+    
+ 
+
+
+
+
+
+
+});
